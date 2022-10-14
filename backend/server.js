a7b02409f3df812aecce543d47c781ad7d4183a8
AHMED-JMD <ahmedabdalrahman706@gmail.com>
2022-10-14 21:16:58 +0200

    start backend

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const app = express();
+
+//connect to db
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => console.log("connected to mongodb"))
+  .catch((err) => console.log("mongodb_err", err));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+const Port = process.env.PORT || 5000;
+app.listen(Port, () => console.log(`server is running on port ${Port}`));
