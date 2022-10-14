e0ddedf3f4cf1955fb08387a74ac0df51baa9808
abdalla <abdallaaymanshandi3@gmail.com>
2022-10-15 01:59:26 +0200

    login done not tested tho

--- backend/controllers/users/userController.js
+++ backend/controllers/users/userController.js
@@ -1,10 +1,35 @@
 const bycryptjs = require("bcryptjs");
-const xssFilter = require("xss-filters");
+const jwt = require("jsonwebtoken");
 const User = require("../../models/user");
+require("dotenv").config();
 
 const user = {
-  login: async (req, res) => {},
-  signup: async (req, res) => {},
+  login: async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      let user = await User.findOne({
+        username,
+      });
+
+      if (!user)
+        res.status(400).send({
+          msg: "user dose not exist",
+        });
+
+      let dosePasswordMatch = await bycryptjs.compare(password, user.password);
+
+      if (!dosePasswordMatch)
+        res.status(401).send({
+          msg: "incorrect password",
+        });
+
+      let token = jwt.sign({ id: user.id }, process.env.JWTSECRET);
+
+      res.send({ token, user });
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 
 module.exports = user;
--- backend/routes/users/auth.js
+++ backend/routes/users/auth.js
@@ -5,7 +5,5 @@ const user = require("../../controllers/users/userController");
 //login route
 router.post("/login", user.login);
 
-//signup route
-router.post("/register", user.signup);
 
 module.exports = router;
